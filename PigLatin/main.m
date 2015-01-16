1774c3e18e0eaa5e6063bd08dcdc8d12b907f29c
4johnny <4johnny@users.noreply.github.com>
2015-01-16 15:13:07 -0800

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Johnny on 2015-01-16.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
